edb4c4eada39a67fc1d1f5bccaeebd84cfab5328
Sid-IRL <85756634+Sid-IRL@users.noreply.github.com>
2026-02-15 23:08:01 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  export default defineConfig({
+  base: '/Sid_Portfolio/',
   server: {
     host: "::",
     port: 8080,
